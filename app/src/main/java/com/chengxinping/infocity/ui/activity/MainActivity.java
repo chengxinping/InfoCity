e887d052361e7966ecea255b906bdbcc644b572c
chengxinping <972649353@qq.com>
2017-03-27 10:41:07 +0800

    添加关于和按两次返回退出程序

--- app/src/main/java/com/chengxinping/infocity/ui/activity/MainActivity.java
+++ app/src/main/java/com/chengxinping/infocity/ui/activity/MainActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -29,6 +32,8 @@ public class MainActivity extends BaseActivity
     NavigationView navigationView;
     @BindView(R.id.fab)
     FloatingActionButton fab;
+
+    private long exitTime = 0;
 
     @Override
     protected void initView() {
@@ -64,16 +69,22 @@ public class MainActivity extends BaseActivity
 
         if (id == R.id.nav_news) {
             setFragment(new NewsFragment());
+            drawer.closeDrawer(GravityCompat.START);
         } else if (id == R.id.nav_bus) {
             setFragment(new BusFragment());
+            drawer.closeDrawer(GravityCompat.START);
         } else if (id == R.id.nav_service) {
             setFragment(new ServiceFragment());
+            drawer.closeDrawer(GravityCompat.START);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_about) {
-
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("智慧城市")
+                    .setMessage("  离开书店的时候，我留下了一把伞，希望拧着它回家的人，是你。")
+                    .setPositiveButton("确定", null)
+                    .show();
         }
-        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
@@ -95,5 +106,24 @@ public class MainActivity extends BaseActivity
 
     public void setFabOnClickListener(View.OnClickListener listener) {
         fab.setOnClickListener(listener);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (System.currentTimeMillis() - exitTime > 2000) {
+                Snackbar snackbar = Snackbar.make(fab, "再按一次退出程序", Snackbar.LENGTH_SHORT);
+//                snackbar.setAction("确定", v -> snackbar.dismiss());
+//                snackbar.setActionTextColor(getResources().getColor(R.color.refresh_color));
+                snackbar.getView().setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                snackbar.show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
